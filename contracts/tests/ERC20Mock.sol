a8c250831296484ccd4c40d0044f6552237311f0
Carlos H. Campello <carlos@qualinfo.com.br>
2023-01-02 09:37:55 -0300

    Decimals in ERC-20 mock token

--- contracts/tests/ERC20Mock.sol
+++ contracts/tests/ERC20Mock.sol
@@ -5,13 +5,21 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract USDPMockTocken is ERC20, Ownable {
-    constructor() ERC20("USDP Mock Tocken", "USDP") {}
+    uint8 customDecimals;
+
+    constructor(uint8 _decimals) ERC20("USDP Mock Tocken", "USDP") {
+        customDecimals = _decimals;
+    }
 
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
-    
+
     function mintToMe(uint256 amount) external {
         _mint(msg.sender, amount);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return customDecimals;
     }
 }
