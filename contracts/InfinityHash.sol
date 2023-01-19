76c2f74eb0e424592eadaba7302819e0e05f1420
Carlos H. Campello <carlos@qualinfo.com.br>
2023-01-19 11:54:46 -0300

    MINTER as immutable and uppercased

--- contracts/InfinityHash.sol
+++ contracts/InfinityHash.sol
@@ -4,15 +4,15 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract InfinityHash is ERC20{
-    address public minter;
+    address public immutable MINTER;
     error NotMinter(address minter, address caller);
 
     constructor(address _minter) ERC20("Infinity Hash", "IFH") {
-        minter = _minter;
+        MINTER = _minter;
     }
 
     function mint(address to, uint256 amount) public {
-        if (msg.sender != minter) revert NotMinter(minter, msg.sender);
+        if (msg.sender != MINTER) revert NotMinter(MINTER, msg.sender);
         _mint(to, amount);
     }
 }
